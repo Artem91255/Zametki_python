45ed35d819d566b348c566dc6588ef8e588bc9d0
Artem.Chernomazov <pechenkazz@mail.ru>
2023-04-13 06:50:37 +0400

    Все заметки сохраняются в один файл и выводятся на экран по запросу

--- controller.py
+++ controller.py
@@ -1,30 +1,34 @@
 import view
 import log
 import datetime
-
+from random import randint
+#идентификатор, заголовок, тело заметки и дату/время создания или последнего изменения заметки
+# Приложение должно запускаться без ошибок, должно уметь сохранять данные
+# в файл, уметь читать данные из файла, делать выборку по дате, выводить на
+# экран выбранную запись, выводить на экран весь список записок, добавлять
+# записку, редактировать ее и удалять
 def add_note():
 
     name = input("Введите название заметки: ")
+    notebody = input("Введите заметку: \n")
 
-    note_name=name.split()
-    name = name + ".csv"
-    #log.write_in_csv(name, note_name)
-
-
-    notebody = input("Введите заметку: \n").split()
-
-    log.write_in_csv(name, notebody)
     notedate = datetime.datetime.now()
-    notedate_str = notedate.strftime("%d %b %Y").split()
-    name_for_list = name.split()
-    #log.write_in_csv(name, notedate_str)
-    log.write_in_list(name_for_list, notedate_str)
+    notedate_str = notedate.strftime("%d %b %Y")
+    random_number =randint(0,10000)
+    list_for_csv = []
+    list_for_csv.append(random_number)
+    list_for_csv.append(name)
+    list_for_csv.append(notebody)
+    list_for_csv.append(notedate_str)
+    log.write_in_list(list_for_csv)
 
 def remove_this_note():
     name_for_delete = input("Введите имя заметки, которую необходимо удалить: ")
     log.delete_csv(name_for_delete)
 
-
+def read_from_csv():
+    # name = input("Введите название заметки: ")
+    log.read_my_csv()
 
 
 
@@ -36,6 +40,9 @@ def button_click_main():
         if x==1:
             add_note()
             print("Заметка успешно создана")
+        elif x == 2:
+            read_from_csv()
+
         elif x == 4:
             remove_this_note()
             print("Заметка успешно удалена.")
--- log.py
+++ log.py
@@ -6,24 +6,34 @@ import controller
 
 
 
-def write_in_csv(name,text):
-    with open(name, 'w', encoding='utf=8') as nts:
-        writer = csv.writer(nts, lineterminator="\r", delimiter=' ')
-
-        writer.writerow(text)
-        writer.writerow("")
+# def write_in_csv(name,text):
+#     with open(name, 'w', encoding='utf=8') as nts:
+#         writer = csv.writer(nts, lineterminator="\r", delimiter=';')
+#
+#         writer.writerow(text)
+#         writer.writerow("")
 
 def delete_csv(name):
     name_to_delete = name+".csv"
     os.remove(name_to_delete)
 
-def write_in_list(name,date):
-    name_for_list = name+date
+def write_in_list(list):
 
     with open('list_of_notes.csv', 'a', encoding='utf=8') as nts:
-        writer = csv.writer(nts, lineterminator="\r", delimiter=' ')
-        writer.writerow(name)
-        writer.writerow(name_for_list)
+        writer = csv.writer(nts, lineterminator="\r", delimiter=';')
+        writer.writerow(list)
+
+def read_my_csv():
+    with open('list_of_notes.csv', 'r', encoding='utf=8') as f:
+        print("")
+        reader = csv.DictReader(f, delimiter=";")
+        for row in reader:
+
+            print(row['name'] + ": "+ row['body'])
+        print("")
+
+
+
 
 
 
